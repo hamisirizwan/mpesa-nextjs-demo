af2c75f66ddd9e8391f48bca0fd9a90ecca43ba1
hamisirizwan <hamisirizwan5@gmail.com>
2024-07-26 13:15:53 +0300

    added padding to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PaymentForm from "@/components/PaymentForm";
 
 export default function Home() {
   return (
-    <section className="bg-gray-100 max-w-400 h-screen flex justify-center items-center px-8">
+    <section className="bg-gray-100 max-w-400 h-screen flex justify-center items-center px-6">
       <PaymentForm />
     </section>
   );
